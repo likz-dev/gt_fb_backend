166e791e83b41783ac5ad95f9b6525630c0b0c8e
Li KangZheng <likzdev@gmail.com>
2021-03-18 12:53:54 +0800

    Updated sql script

--- sql_scripts/script.sql
+++ sql_scripts/script.sql
@@ -1,4 +1,4 @@
-create table if not exists gt.facility
+create table if not exists facility
 (
     id    serial  not null
         constraint facilities_pk
@@ -8,51 +8,34 @@ create table if not exists gt.facility
     pax   integer not null
 );
 
-alter table gt.facility
+alter table facility
     owner to likz;
 
 create unique index if not exists facilities_id_uindex
-    on gt.facility (id);
+    on facility (id);
 
 create unique index if not exists facilities_name_uindex
-    on gt.facility (name);
+    on facility (name);
 
-create table if not exists gt.fb_user
+create table if not exists booking
 (
-    user_id serial not null,
-    name    text   not null
-);
-
-alter table gt.fb_user
-    owner to likz;
-
-create unique index if not exists user_name_uindex
-    on gt.fb_user (name);
-
-create unique index if not exists user_user_id_uindex
-    on gt.fb_user (user_id);
-
-create table if not exists gt.booking
-(
-    booking_id  serial not null
+    booking_id   serial not null
         constraint booking_pk
             primary key,
-    start_time  timestamp,
-    end_time    timestamp,
-    booked_by   text
-        constraint booking_user_name_fk
-            references gt.fb_user (name),
-    facility_id integer
+    start_time   timestamp,
+    end_time     timestamp,
+    booked_by    text,
+    facility_id  integer
         constraint booking_facility_id_fk
-            references gt.facility,
-    name        text
+            references facility,
+    booking_name text
 );
 
-alter table gt.booking
+alter table booking
     owner to likz;
 
 create unique index if not exists booking_booking_id_uindex
-    on gt.booking (booking_id);
+    on booking (booking_id);
 
 create unique index if not exists booking_booking_id_uindex_2
-    on gt.booking (booking_id);
+    on booking (booking_id);
